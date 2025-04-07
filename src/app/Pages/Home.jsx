1b1f9307b5ed4c556d16ff00bc15261969260675
Indradeep Mandal <screechin_03@Indradeeps-MacBook-Air.local>
2025-04-07 12:50:32 +0530

    imahes new

--- src/app/Pages/Home.jsx
+++ src/app/Pages/Home.jsx
@@ -43,11 +43,13 @@ export default function Home() {
             <div className="absolute top-0 left-0 w-full h-full bg-[#063254]/70 z-[1] pointer-events-none"></div>
 
             {/* 🔹 Background Floating Elements */}
-            <img src="/Group 85.png" alt="Background Element" className="w-8 absolute top-35 left-28 h-8 z-[2]" />
-            <img src="/Group 87.png" alt="Background Element" className="w-7 absolute top-75 right-67 h-7 z-[2]" />
-            <img src="/Group 86.png" alt="Background Element" className="w-6 absolute top-103 right-157 h-6 z-[2]" />
-            <img src="/Comet.svg" alt="Background Element" className="w-34 absolute top-20 right-24 h-34 z-[2]" />
-            <img src="/Group 129.svg" alt="Background Element" className="w-10 absolute top-140 right-24 h-10 opacity-80 z-[2]" />
+            <img src="/Group 85.png" alt="Background Element" className="w-8 absolute top-35 left-28 h-8 z-[2] hidden sm:block" />
+            <img src="/Group 85.png" alt="Background Element" className="w-7 absolute top-51 left-3 h-7 z-[2] block sm:hidden" />
+            <img src="/Group 85.png" alt="Background Element" className="w-6 absolute top-65 right-3 h-6 z-[2] block sm:hidden" />
+            <img src="/Group 87.png" alt="Background Element" className="w-7 absolute top-75 right-67 h-7 z-[2]  hidden sm:block" />
+            <img src="/Group 86.png" alt="Background Element" className="w-6 absolute top-103 right-157 h-6 z-[2] hidden sm:block" />
+            <img src="/Comet.svg" alt="Background Element" className="w-34 absolute top-20 right-24 h-34 z-[2] hidden sm:block" />
+            <img src="/Group 129.svg" alt="Background Element" className="w-10 absolute top-140 right-24 h-10 opacity-80 z-[2] hidden sm:block" />
 
             {/* Navbar */}
             <div className="relative z-[3]">
@@ -61,13 +63,22 @@ export default function Home() {
                     <title>Status Code 2</title>
                 </Head>
 
-                <img src="/Vector (1).svg" className="mb-4" />
-                <p className="mt-2 text-2xl bg-gradient-to-r from-[#BAF7FF] to-white text-transparent bg-clip-text">
+                <>
+                    {/* Desktop & tablet image */}
+                    <img src="/Vector (1).svg" className="hidden sm:block mb-4" alt="Desktop Vector" />
+
+                    {/* Mobile-only image */}
+                    <img src="/Vector (4).png" className="block sm:hidden mb-4 mt-9" alt="Mobile Vector" />
+                </>
+
+                <p className="mt-2 text-base sm:text-xl md:text-xl lg:text-2xl bg-gradient-to-r from-[#BAF7FF] to-white text-transparent bg-clip-text sm:font-medium sm:mb-7">
                     Code with Purpose & Innovate, Collaborate, Dominate
                 </p>
 
+
+
                 {/* CTA Button */}
-                <button className="mt-4 relative z-[3]">
+                <button className="mt-4 relative z-[3] sm:mt-9">
                     <img
                         src="/Cta Button.png"
                         alt="Call to Action"
@@ -77,16 +88,21 @@ export default function Home() {
 
                 {/* Floating Ghost + More Elements */}
                 <div className="-mt-25 relative flex flex-col items-center justify-center w-full z-[3]">
-                    <div className="absolute inset-0 w-full h-full z-[2]">
-                        <img src="/Group 58.png" alt="Background Element" className="w-full absolute top-74 h-auto opacity-50" />
-                        <img src="/Group 56.png" alt="Background Element" className="w-37 h-37 absolute top-70 left-42 opacity-90" />
-                        <img src="/Group 131.png" alt="Background Element" className="w-23 h-30 absolute top-80 right-97 opacity-80" />
-                        <img src="/Group 134.svg" alt="Background Element" className="w-34 h-34 absolute top-53 right-122 opacity-100" />
-                        <img src="/Group 136.png" alt="Background Element" className="w-25 h-20 absolute top-98 right-110 opacity-80" />
-                        <img src="/Group 56.png" alt="Background Element" className="w-20 h-15 absolute top-84 left-110 opacity-80" />
-                        <img src="/Group 57.png" alt="Background Element" className="w-32 h-32 absolute top-84 right-30 mix-blend-difference opacity-80 " />
+                    <div className="flex flex-col absolute inset-0 w-full h-full z-[2]">
+                        <p className="mt-28 bg-gradient-to-r from-[#A5FECB] to-[#A5FECB] via-[#92EFFD] text-transparent bg-clip-text  font-ticketing mx-8 text-center opacity-100 text-base absolute z-20 block sm:hidden">
+                            Unleash your creativity and join Status Code 2, the ultimate 36-hour hackathon extravaganza! Compete across diverse categories, showcasing your skills, ingenuity, and collaborative spirit alongside like-minded individuals. Discover the perfect platform to challenge yourself.
+                        </p>
+                        <img src="/Group 58.png" alt="Background Element" className="w-full absolute top-74 h-auto opacity-50 hidden sm:block" />
+                        <img src="/Group 56.png" alt="Background Element" className="w-37 h-37 absolute top-70 left-42 opacity-90 hidden sm:block" />
+                        <img src="/Group 131.png" alt="Background Element" className="w-23 h-30 absolute top-80 right-97 opacity-80 hidden sm:block" />
+                        <img src="/Group 134.svg" alt="Background Element" className="w-34 h-34 absolute top-53 right-122 opacity-100 hidden sm:block" />
+                        <img src="/Group 136.png" alt="Background Element" className="w-25 h-20 absolute top-98 right-110 opacity-80 hidden sm:block" />
+                        <img src="/Group 56.png" alt="Background Element" className="w-20 h-15 absolute top-84 left-110 opacity-80 hidden sm:block" />
+                        <img src="/Group 57.png" alt="Background Element" className="w-32 h-32 absolute top-84 right-30 mix-blend-difference opacity-80 hidden sm:block" />
+                        <img src="/Rectangle 3.png" className="absolute z-6 w-full right-0 opacity-100 h-60 top-87 hidden sm:block " />
 
-                        <p className="mt-128 bg-gradient-to-r from-[#A5FECB] to-[#A5FECB] via-[#92EFFD] text-transparent bg-clip-text text-xl font-ticketing mx-34 text-center opacity-100 absolute z-10 ">
+
+                        <p className="mt-128 bg-gradient-to-r from-[#A5FECB] to-[#A5FECB] via-[#92EFFD] text-transparent bg-clip-text text-xl font-ticketing mx-34 text-center opacity-100 absolute z-20 hidden sm:block">
                             Unleash your creativity and join Status Code 2, the ultimate 36-hour hackathon extravaganza! Compete across diverse categories, showcasing your skills, ingenuity, and collaborative spirit alongside like-minded individuals. Discover the perfect platform to challenge yourself.
                         </p>
 
@@ -97,7 +113,7 @@ export default function Home() {
                     <img
                         src="/Levitaing Ghost.svg"
                         alt="Levitating Ghost"
-                        className="w-135 h-130 animate-[wiggle_5s_ease-in-out_infinite] relative z-[3]"
+                        className="w-135 h-130  top-40 animate-[wiggle_5s_ease-in-out_infinite] relative z-[3]"
                     />
                 </div>
 
--- src/app/Pages/Second.jsx
+++ src/app/Pages/Second.jsx
@@ -23,34 +23,54 @@ const Second = () => {
     };
 
     return (
-        <div className="relative w-full overflow-hidden ">
-            {/* Background Image */}
-            <img src="/Group 81.png" alt="Background Element" className="w-full h-auto opacity-100 mt-50 relative z-10" />
+        <>
+            <div className="relative w-full overflow-hidden hidden sm:block ">
+                <img src="/Group 81.png" alt="Background Element" className="w-full h-auto opacity-100 mt-50 relative z-10 " />
+                <div className="absolute top-97 left-1/2 transform -translate-x-1/2 text-white text-[170px] z-1 font-bold px-4 py-2 rounded hidden sm:block">
+                    {formatTime(time)}
+                </div>
+                <div className="absolute z-10 top-77 left-156 text-red-500 font-light flex flex-row text-6xl space-x-36 items-center ">
+                    HURRY UP!
+                </div>
+                <div className="absolute z-10 top-89 left-130 text-white font-light flex flex-row text-6xl space-x-36 items-center">
+                    HACKING STARTS IN...
+                </div>
+                <div className="absolute z-10 top-137 left-116 text-red-500 font-light flex flex-row text-4xl space-x-36 items-center">
+                    <p>DAYS</p>
+                    <p>HOURS</p>
+                    <p>MINUTES</p>
+                </div>
+                <div className="lg:overflow-hidden">
+                    <img src="/rotate=0.svg" className="h-340 w-340 absolute -top-10 left-1/2 transform -translate-x-1/2 z-2 spin overflow-hidden pt-8" />
+                    <img src="/rotate=120.png" className="h-260 w-260 absolute top-20 left-1/2 transform -translate-x-1/2 z-2 spin-slow overflow-hidden mt-7" />
+                    <img src="/rotate=0.svg" className="h-240 w-240 absolute top-20 left-1/2 transform -translate-x-1/2 z-2 spin overflow-hidden pt-4" />
+                </div>
+            </div>
+            <div className="relative w-full  block sm:hidden mb-49">
+                <img src="/Group 81.png" alt="Background Element" className="w-full h-auto opacity-100 mt-50 relative z-10 hidden sm:block" />
+                <div className="absolute top-43 left-1/2 transform -translate-x-1/2 text-white text-5xl z-1 font-bold px-4 py-2 rounded ">
+                    {formatTime(time)}
+                </div>
+                <div className="absolute z-10 top-27 left-1/2 text-red-500 font-light transform  -translate-x-1/2 flex flex-row text-xl  items-center ">
+                    HURRY UP!
+                </div>
+                <div className="absolute z-10 top-33 left-1/2 text-white  font-light transform -translate-x-1/2 flex flex-row text-xl space-x-36 items-center">
+                    HACKING STARTS IN...
+                </div>
+                <div className="absolute z-10 top-54 left-63 text-red-500 font-light transform -translate-x-1/2 flex flex-row text-lg space-x-5 items-center">
+                    <p>DAYS</p>
 
-            {/* Digital Clock Display */}
-            <div className="absolute top-97 left-1/2 transform -translate-x-1/2 text-white text-[170px] z-1 font-bold px-4 py-2 rounded">
-                {formatTime(time)}
-            </div>
-            <div className="absolute z-10 top-77 left-156 text-red-500 font-light flex flex-row text-6xl space-x-36 items-center">
-                HURRY UP!
-            </div>
-            <div className="absolute z-10 top-89 left-130 text-white font-light flex flex-row text-6xl space-x-36 items-center">
-                HACKING STARTS IN...
-            </div>
-            <div className="absolute z-10 top-137 left-116 text-red-500 font-light flex flex-row text-4xl space-x-36 items-center">
-                <p>DAYS</p>
-                <p>HOURS</p>
-                <p>MINUTES</p>
-            </div>
+                    <p>HOURS</p>
 
-            {/* Rotating Elements */}
-            <div className="overflow-hidden">
-                <img src="/rotate=0.svg" className="h-340 w-340 absolute -top-13 left-1/2 transform -translate-x-1/2 z-2 spin overflow-hidden pt-8" />
-                <img src="/rotate=120.png" className="h-260 w-260 absolute top-20 left-1/2 transform -translate-x-1/2 z-2 spin-slow overflow-hidden mt-7" />
-                <img src="/rotate=0.svg" className="h-240 w-240 absolute top-20 left-1/2 transform -translate-x-1/2 z-2 spin overflow-hidden pt-4" />
+                    <p>MINUTES</p>
+                </div>
+                <div className="lg:overflow-hidden">
+                    <img src="/rotate=0.svg" className="h-[480px] w-[480px] absolute -top-14  left-62 transform -translate-x-1/2 z-2 spin overflow-hidden " />
+                    <img src="/rotate=120.png" className="h-[360px] w-[360px] absolute top-0 left-1/2 transform -translate-x-1/2 z-2 spin-slow overflow-hidden " />
+                    <img src="/rotate=0.svg" className="h-[320px] w-[320px] absolute top-5 left-1/2 transform -translate-x-1/2 z-2 spin overflow-hidden " />
+                </div>
             </div>
-
-        </div>
+        </>
     );
 };
 
