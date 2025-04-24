3fd0b298094bfc4f347e8fef5514881b2f1b8700
Waqas-1114 <waqasomar2@gmail.com>
2025-04-24 12:55:49 +0530

    Render

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    devIndicators: false
+    output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
