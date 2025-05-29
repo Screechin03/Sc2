297639c0560843abfe7e892c4f46a3f4f324ea8d
Waqas-1114 <waqasomar2@gmail.com>
2025-05-29 18:31:34 +0530

    Updated package.json and next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output: 'export',
-//   images: {
-//     unoptimized: true,
-//   },
-//   basePath: process.env.BASE_PATH || '',
-//   devIndicators: false,
-// };
-
-// export default nextConfig;
-
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+    output: 'export',
 };
 
 export default nextConfig;
-
