1077878f12b593025a777be5f5f8b1aa1cc1e9a6
Waqas-1114 <waqasomar2@gmail.com>
2025-07-22 13:40:06 +0530

    Bug fixes

--- src/app/Pages/Home.jsx
+++ src/app/Pages/Home.jsx
@@ -203,7 +203,6 @@ export default function Home() {
 
       <div className="mt-20 md:mt-40"></div>
       <Faqs />
-      <Marquee />
 
       <div className="mt-20 md:mt-40"></div>
       <Last />
